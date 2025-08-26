9467c1168460dc1db3dc58699dc4201e1811946b
Anitha Yadav <131986444+muthuani@users.noreply.github.com>
2025-08-26 16:47:40 +0400

    Add files via upload

---
+++ basics/loops.py
@@ -0,0 +1,12 @@
+# Loops in Python
+# A loop allows us to repeat a block of code multiple times.
+
+# Example: For loop
+for i in range(5):
+    print("Iteration:", i)
+
+# Example: While loop
+count = 0
+while count < 3:
+    print("Count is:", count)
+    count += 1
