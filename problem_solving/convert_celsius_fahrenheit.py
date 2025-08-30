497f35196d4c858b2829eef0e093ed0e1ff3452e
Anitha Yadav <131986444+muthuani@users.noreply.github.com>
2025-08-30 18:04:08 +0400

    Create convert_celsius_fahrenheit.py

---
+++ problem_solving/convert_celsius_fahrenheit.py
@@ -0,0 +1,4 @@
+# Python Program to take the temperature in Celsius and convert it to Fahrenheit
+celsius=float(input("Please Enter the temperature in Celsius : "))
+fahrenheit = (celsius * 1.8 ) + 32
+print(f"Temperature in Fahrenheit is {fahrenheit}")
